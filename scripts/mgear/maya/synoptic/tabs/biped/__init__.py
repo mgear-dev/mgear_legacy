7ab2353bf13c95302974eb006987ffaaeb7eaff1
Villar Rafael <rafael@studioanima.co.jp>
2018-08-03 15:03:22 +0900

    Backporting callbackManager
    Removed scriptjobs from Synoptic tabs and replaced with callback manager.

--- scripts/mgear/maya/synoptic/tabs/biped/__init__.py
+++ scripts/mgear/maya/synoptic/tabs/biped/__init__.py
@@ -28,6 +28,7 @@ class SynopticTab(MainSynopticTab, widget.Ui_biped_body):
     # INIT
     def __init__(self, parent=None):
         super(SynopticTab, self).__init__(self, parent)
+        self.cbManager.selectionChangedCB(self.name, self.selectChanged)
 
     # ============================================
     # BUTTONS
